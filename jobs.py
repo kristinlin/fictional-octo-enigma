b7e6f06ab5798799dc7fb10750b5893db39d3c83
kristinlin <klin11@stuy.edu>
2017-09-15 14:44:44 -0400

    general thing is there. probably needs tweaks

--- jobs.py
+++ jobs.py
@@ -1,4 +1,6 @@
 import csv
+import random
+
 JOBS = dict()
 
 def readFile() :
@@ -8,11 +10,21 @@ def readFile() :
         for row in reader:
             JOBS.update({row['Job Class']:
                          float(row['Percentage'])})
-    
+#        del JOBS["Total"]
 
-            
-            
+def randomJob() :
+    select = random.random() * JOBS["Total"];
+    for x in JOBS :
+        if x == "Total" :
+            hey = "ignore me"
+        elif (select < 0) :
+            return x
+        else :
+            select = select - JOBS[x]
+                    
 readFile()
 print JOBS
+print(randomJob())
+#print JOBS
 
 
