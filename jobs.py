5591cb00b635c800fff22f719eae09b8b16f5174
kristinlin <klin11@stuy.edu>
2017-09-16 12:07:17 -0400

    added comments and changed randomJob() method a little

--- jobs.py
+++ jobs.py
@@ -1,7 +1,17 @@
+# Fabiha Ahmed & Kristin Lin
+# SoftDev pd09
+# Work03 : Files Occupy Space
+# 09-15-17
+
 import csv
 import random
 
+# create dictionary
 JOBS = dict()
+
+
+# reads file occupations.csv as csvfile and updates JOBS with Job class as key
+# and percentage as value typecasted as a float.
 
 def readFile() :
     with open('occupations.csv') as csvfile:
@@ -10,21 +20,34 @@ def readFile() :
         for row in reader:
             JOBS.update({row['Job Class']:
                          float(row['Percentage'])})
-#        del JOBS["Total"]
+
+            
+# Total is 99.8, so any number from 0 to 99.8, not included, is randomly chosen
+# We imagined that each of the sections actually held a portion of that 99.8.
+# For example, Protective Services is 2.8 percent, but it could represent 34.7
+# to 37.5 percent of 99.8 or 71.2 to 74 percent of 99.8.
+# Therefore, we used that random number and continuously subtracted each key
+# value until the random number became negative, which means that that key is
+# chosen
 
 def randomJob() :
     select = random.random() * JOBS["Total"];
+    # print select
     for x in JOBS :
         if x == "Total" :
-            hey = "ignore me"
-        elif (select < 0) :
+            select = select
+            # nothing happens; no one cares about total
+        elif (select - JOBS[x] < 0) :
             return x
         else :
             select = select - JOBS[x]
+
+
+#-------------------------------------------
                     
 readFile()
-print JOBS
-print(randomJob())
 #print JOBS
+print(randomJob())
+
 
 
